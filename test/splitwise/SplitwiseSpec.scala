c7efb2fc14b61a5c858cd99a4b4c77da74b1ed72
Iona Vincent <ionagvincent@gmail.com>
2016-08-24 22:42:39 +0100

    Add test for JSON deserialisation

---
+++ test/splitwise/SplitwiseSpec.scala
@@ -0,0 +1,28 @@
+package splitwise
+
+import org.scalatest.{FlatSpec, Matchers}
+import play.api.libs.json.{JsResult, JsSuccess}
+import play.api.libs.json.Json.parse
+import services.SplitwiseFormats.Expenses
+
+import scala.io.Source
+
+class SplitwiseSpec extends FlatSpec with Matchers {
+
+  it should "deserialise JSON" in {
+     val json = parse(
+        Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("splitwise/expenses.json")).mkString
+      )
+      json.validate[Expenses] match {
+        case JsSuccess(expenses, _) => {
+          val firstExpense = expenses.expenses.find(_.id == 114828821).get
+          firstExpense.cost shouldBe 10.44
+          firstExpense.currency_code shouldBe "GBP"
+          firstExpense.creation_method shouldBe Some("split")
+          firstExpense.description shouldBe "Book"
+        }
+        case _ => fail("Could not parse JSON for expenses")
+      }
+  }
+
+}
